2c57940147d5a9a1e39f80b5c59e235d3571784c
sobird <i@sobird.me>
2023-11-22 22:31:34 +0800

    do not use rollup-plugin-esbuild

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,7 +10,7 @@ import { dirname, relative, extname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
-import esbuild from 'rollup-plugin-esbuild';
+import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import copy from 'rollup-plugin-copy';
@@ -72,8 +72,8 @@ export default (env) => {
           preferBuiltins: true
         }),
         commonjs(),
-        esbuild({
-          // check: false,
+        typescript({
+          check: false,
           // declaration: true,
           // tsconfig: "./src/tsconfig.json",
           // noEmitOnError: false,
