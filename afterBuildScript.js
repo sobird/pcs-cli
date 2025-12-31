3f398862b878fc360875b93c6b88b5182d8e5325
sobird <i@sobird.me>
2025-12-31 20:08:27 +0800

    chore: upgrade deps

--- afterBuildScript.js
+++
@@ -1,7 +0,0 @@
-// rollup watch 模式 导致bin失去x权限
-import { openSync, fchmodSync, constants } from 'fs';
-
-console.log('constants', constants.S_IRWXU, constants.S_IRGRP);
-
-const fd = openSync('./dist/bin/pcs.js', 'r');
-fchmodSync(fd, constants.S_IRWXU | constants.S_IRGRP | constants.S_IXGRP | constants.S_IXOTH);
